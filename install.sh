004d1f3ac8d1c34b41697ae60b0066f2c4cb909e
Harsh Kanjariya <code.harshkanjariya@gmail.com>
2023-02-04 13:05:28 +0530

    initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -r node_modules
+rm package-lock.json
+npm i
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ppb=$(pwd)/penpencil-backend
+if [ ! -d "$ppb" ]; then
+	echo "penpencil-backend folder not found"
+	exit 1
+fi
+git clone git@github.com:harshkanjariya/pp-backend-setup.git
+cd pp-backend-setup
+docker build -t pp-builder -f pp-builder.docker .
+cd ..
+docker run --name pp-builder -d -v $ppb:/app pp-builder
+
+rm -r $ppb/node_modules
+rm $ppb/package-lock.json
+cd $ppb
+docker exec -it pp-builder "cd /app && npm i"
